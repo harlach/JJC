9c9e6c8a53239d38dcdad3a5f62349e2caa8a97b
Chris Harland <31423720+harlach@users.noreply.github.com>
2017-11-19 19:15:13 -0600

    Health Bar

---
+++ TutorialACTtrans.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialACTtrans : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "transitionAct1")
+        {
+            SceneManager.LoadScene("TutorialtoAct1");
+        }
+    }
+}
